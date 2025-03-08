756ff4bcc715c4eb2e00f5cb6d963e917f73cc19
Pedro Moreno Fernández <pmoreno@iesemilidarder.com>
2025-03-08 12:42:55 +0100

    v1.1. Añadido modificacion y eliminacion de registros

--- routes/api.php
+++ routes/api.php
@@ -24,10 +24,24 @@ Route::middleware(['api'])->group(function () {
     Route::post('autores', [AutorController::class, 'store']);
     Route::post('prestamos', [PrestamoController::class, 'store']);
     Route::post('reservas', [ReservaController::class, 'store']);
+    // Endpoints para eliminar un recurso especifico
+    Route::delete('usuarios/del/{id}', [UsuarioController::class, 'destroy']);
+    Route::delete('libros/del/{id}', [LibroController::class, 'destroy']);
+    Route::delete('autores/del/{id}', [AutorController::class, 'destroy']);
+    Route::delete('prestamos/del/{id}', [PrestamoController::class, 'destroy']);
+    Route::delete('reservas/del/{id}', [ReservaController::class, 'destroy']);
+
     // Endpoints para ver un recurso especifico
     Route::get('usuarios/{id}', [UsuarioController::class, 'show']);
     Route::get('libros/{id}', [LibroController::class, 'show']);
     Route::get('autores/{id}', [AutorController::class, 'show']);
     Route::get('prestamos/{id}', [PrestamoController::class, 'show']);
     Route::get('reservas/{id}', [ReservaController::class, 'show']);
+
+    // Endpoints para modificar un recurso especifico
+    Route::put('usuarios/mod/{id}', [UsuarioController::class, 'update']);
+    Route::put('libros/mod/{id}', [LibroController::class, 'update']);  
+    Route::put('autores/mod/{id}', [AutorController::class, 'update']);
+    Route::put('prestamos/mod/{id}', [PrestamoController::class, 'update']);
+    Route::put('reservas/mod/{id}', [ReservaController::class, 'update']);
 });
